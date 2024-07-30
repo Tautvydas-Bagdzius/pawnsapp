4902bf306fa118f6c8b771a0ac2f51ddc76e8cf1
Tautvydas-Bagdzius <tautvydas.bagdzius@gmail.com>
2024-07-30 12:38:06 +0300

    Breeze/Sanctum added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
+
+
+/**
+ * For demonstration purposes
+ */
+Route::get('csrf', function () {
+    return ['CSRF' => csrf_token(), 'user_name' => Auth::user()?->name];
+});
+
+
+require __DIR__.'/auth.php';
